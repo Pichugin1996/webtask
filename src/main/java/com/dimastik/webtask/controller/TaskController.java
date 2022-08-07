254af9161ca9f05fe0dfc938996c039083f21d7b
Dima <diman-pichugin@yandex.ru>
2022-08-07 18:22:17 +0300

    Добавлена возможность сохранения, редактирования задачи

--- src/main/java/com/dimastik/webtask/controller/TaskController.java
+++ src/main/java/com/dimastik/webtask/controller/TaskController.java
@@ -1,14 +1,20 @@
 package com.dimastik.webtask.controller;
 
+import com.dimastik.webtask.model.Task;
 import com.dimastik.webtask.service.TaskService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
 
 @Controller
+@Slf4j
 public class TaskController {
 
     private final TaskService taskService;
@@ -24,6 +30,33 @@ public class TaskController {
         model.addAttribute("tasks", taskService.getAllTasksByUserNameId(principal.getName()));
         return "task/manager";
     }
+
+    @GetMapping("/task/{id}")
+    public String getTaskById(@PathVariable Long id, Model model, Principal principal) {
+        model.addAttribute("task", taskService.getTaskById(id, principal.getName()));
+        return "/task/task";
+    }
+
+    @GetMapping("/task/taskEditor/{id}")
+    public String editor(@PathVariable Long id, Model model, Principal principal) {
+        Task taskById = taskService.getTaskById(id, principal.getName());
+        model.addAttribute("task", taskById);
+        return "/task/taskEditor";
+    }
+
+    @PostMapping("/task/taskEditor")
+    public String editorSave(@ModelAttribute("task") Task task, Model model, Principal principal) {
+        taskService.saveTask(task, principal);
+        model.addAttribute("save", "Сохранено!");
+        return "/task/taskEditor";
+    }
+
+    @GetMapping("task/taskEditor")
+    public String editor(Model model) {
+        model.addAttribute("task", new Task());
+        return "/task/taskEditor";
+    }
+
 
 
 }
--- src/main/java/com/dimastik/webtask/service/TaskService.java
+++ src/main/java/com/dimastik/webtask/service/TaskService.java
@@ -1,12 +1,13 @@
 package com.dimastik.webtask.service;
 
 import com.dimastik.webtask.model.Task;
+import com.dimastik.webtask.model.User;
 import com.dimastik.webtask.repository.TaskRepository;
 import com.dimastik.webtask.repository.UserRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,9 +24,36 @@ public class TaskService {
     }
 
     public List<Task> getAllTasksByUserNameId(String userName) {
-        List<Optional<Task>> optionals = taskRepository.findByUserNameId(userRepository.findByUsername(userName).get().getId());
+        List<Optional<Task>> optionals = taskRepository.findByUserNameId(getUserNameId(userName));
         List<Task> list = optionals.stream().map(task -> task.get()).toList();
         return list;
     }
 
+
+    public Task getTaskById(Long id, String username) {
+        Task task = taskRepository.findById(id).orElse(new Task());
+        if (task.getUserNameId() != getUserNameId(username)) {
+            return new Task();
+        }
+        return task;
+    }
+
+    public void saveTask(Task task, Principal principal) {
+        Long userNameId = getUserNameId(principal.getName());
+        //Содержится ли в списке пользователя задача с текущим id если да, то сохраняем
+        if (getAllTasksByUserNameId(principal.getName())
+                .stream().map(task1 -> task1.getId()).toList()
+                .contains(task.getId())) {
+            task.setUserNameId(userNameId);
+            taskRepository.save(task);
+        } else {
+            task.setId(null);
+            task.setUserNameId(userNameId);
+            taskRepository.save(task);
+        }
+    }
+
+    private Long getUserNameId(String userName) {
+        return userRepository.findByUsername(userName).get().getId();
+    }
 }
