38cf7cb828fd9146185da5d8558821ba95b7d93b
Dima <diman-pichugin@yandex.ru>
2022-08-09 15:34:08 +0300

    Добавлен README

---
+++ README.md
@@ -0,0 +1,42 @@
+<h1>Веб приложение для хранение задач пользователя (CRUD)</h1>
+Ver. 1.1
+
+Автор Пичугин Дмитрий
+
+Для скачивания репозитория введите команду:
+```
+git clone git@github.com:Pichugin1996/webtask.git
+```
+Возможности:
+
+Регистрация пользователя
+<details>
+<summary>Изображение</summary>
+![](img/register.jpg)
+</details>
+
+Авторизация пользователя
+<details>
+<summary>Изображение</summary>
+![](img/login.jpg)
+</details>
+
+
+Смена пароля у пользователя
+<details>
+<summary>Изображение</summary>
+![](img/repass.jpg)
+</details>
+
+Создание, редактирование, удаление задачи
+<details>
+<summary>Изображение</summary>
+![](img/manager.jpg)
+![](img/taskeditor.jpg)
+</details>
+
+Создания пользователя с ролью администратора, который может выдавать роли из админ панели
+<details>
+<summary>Изображение</summary>
+![](img/adminpanel.jpg)
+</details>
--- src/main/java/com/dimastik/webtask/controller/WebController.java
+++ src/main/java/com/dimastik/webtask/controller/WebController.java
@@ -1,21 +1,10 @@
 package com.dimastik.webtask.controller;
 
-import com.dimastik.webtask.model.User;
-import com.dimastik.webtask.service.UserService;
-import com.dimastik.webtask.validate.RegisterFormValid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 import java.security.Principal;
 
 @Controller
@@ -23,7 +12,10 @@ import java.security.Principal;
 public class WebController {
 
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(Model model, Principal principal) {
+        if (principal != null) {
+            model.addAttribute("username", principal.getName());
+        }
         return "home";
     }
 
